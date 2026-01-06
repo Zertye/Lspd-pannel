1946401e95c8cf92d8dc525eef045b201679bb97
Zertye <nael23.jf@gmail.com>
2026-01-06 12:15:04 +0800

    Delete backend/middleware/upload.js

--- backend/middleware/upload.js
+++
@@ -1,8 +0,0 @@
-const multer = require("multer");
-const storage = multer.memoryStorage();
-const fileFilter = (req, file, cb) => {
-  if (/jpeg|jpg|png|webp/.test(file.mimetype)) cb(null, true);
-  else cb(new Error("Format invalide."));
-};
-const upload = multer({ storage, limits: { fileSize: 3 * 1024 * 1024 }, fileFilter });
-module.exports = upload;
