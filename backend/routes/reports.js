dccc1096ad8129258e36bfbc57dfe7ed55ca7fb6
Zertye <nael23.jf@gmail.com>
2026-01-06 12:13:26 +0800

    Delete backend/routes directory

--- backend/routes/reports.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-const router = express.Router();
-// Route inactive pour LSPD mais nécessaire pour structure
-router.get("/", (req, res) => res.json([]));
-module.exports = router;
